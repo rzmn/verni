a78acff8ebce99a60509397bc9144b1415bb7832
rzmn <nrazumnyi@gmail.com>
2024-07-16 20:16:10 +0300

    added temporary migration items

--- server/cmd/accounty/main.go
+++ server/cmd/accounty/main.go
@@ -4,13 +4,13 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/gin-gonic/gin"
-
 	"accounty/internal/config"
 	"accounty/internal/http-server/router/auth"
 	"accounty/internal/http-server/router/friends"
 	"accounty/internal/http-server/router/users"
 	"accounty/internal/storage/sqlite"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
@@ -37,3 +37,35 @@ func main() {
 
 	server.ListenAndServe()
 }
+
+// type MigrationSpendingItem struct {
+// 	Date        string  `json:"Date"`
+// 	Description string  `json:"Description"`
+// 	Category    string  `json:"Category"`
+// 	Cost        float32 `json:"Cost"`
+// 	Currency    string  `json:"Currency"`
+// 	Margo       float32 `json:"margo"`
+// 	Rzmn        float32 `json:"rzmn"`
+// }
+
+// func migrate(db *sqlite.Storage) {
+// 	jsonFile, err := os.Open("./data/migration.json")
+// 	if err != nil {
+// 		fmt.Println(err)
+// 		return
+// 	}
+// 	fmt.Println("Successfully Opened users.json")
+// 	defer jsonFile.Close()
+// 	byteValue, _ := ioutil.ReadAll(jsonFile)
+// 	var items []MigrationSpendingItem
+// 	json.Unmarshal(byteValue, &items)
+// 	for i := 0; i < len(items); i++ {
+// 		format := "2006-01-02"
+// 		t, err := time.Parse(format, items[i].Date)
+// 		if err != nil {
+// 			fmt.Printf("time parse failed %v\n", err)
+// 			return
+// 		}
+// 		fmt.Printf("%s, %v\n", items[i].Date, t)
+// 	}
+// }
