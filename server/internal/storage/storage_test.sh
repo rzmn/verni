6efe0b1659005e750caff88a4226b30515dde436
rzmn <nrazumnyi@gmail.com>
2024-07-29 07:00:10 +0300

    fixed tests

--- server/internal/storage/storage_test.sh
+++ server/internal/storage/storage_test.sh
@@ -1,7 +1,15 @@
 set -e -x
 
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
 mkdir -p ~/ydbd && cd ~/ydbd
 curl https://install.ydb.tech | bash
 
-./start.sh ram &
+./start.sh ram
+
+CURRENT_DIR=$(pwd)
+cd "${SCRIPT_DIR}"
 go test -v .
+cd "${CURRENT_DIR}"
+
+./stop.sh
