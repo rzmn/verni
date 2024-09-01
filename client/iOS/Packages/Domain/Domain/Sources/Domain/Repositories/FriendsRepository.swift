69a9d5267b13a65edd5d184d00aa786486c28fe0
rzmn <nrazumnyi@gmail.com>
2024-09-01 20:51:08 +0300

    sendable properties

--- client/iOS/Packages/Domain/Domain/Sources/Domain/Repositories/FriendsRepository.swift
+++ client/iOS/Packages/Domain/Domain/Sources/Domain/Repositories/FriendsRepository.swift
@@ -1,34 +1,5 @@
 import Combine
 
-public enum FriendshipKind: Int, CaseIterable, Hashable {
-    case friends
-    case incoming
-    case pending
-}
-
-public struct FriendshipKindSet: OptionSet, Hashable {
-    public static let friends = FriendshipKindSet(rawValue: 1 << FriendshipKind.friends.rawValue)
-    public static let incoming = FriendshipKindSet(rawValue: 1 << FriendshipKind.incoming.rawValue)
-    public static let pending = FriendshipKindSet(rawValue: 1 << FriendshipKind.pending.rawValue)
-
-    public static var all: FriendshipKindSet {
-        FriendshipKind.allCases.reduce(into: []) { set, value in
-            set.insert(FriendshipKindSet(rawValue: 1 << value.rawValue))
-        }
-    }
-
-    public var array: [FriendshipKind] {
-        FriendshipKind.allCases.filter {
-            contains(FriendshipKindSet(rawValue: 1 << $0.rawValue))
-        }
-    }
-
-    public var rawValue: Int
-    public init(rawValue: Int) {
-        self.rawValue = rawValue
-    }
-}
-
 public protocol FriendsRepository {
     @discardableResult
     func refreshFriends(
