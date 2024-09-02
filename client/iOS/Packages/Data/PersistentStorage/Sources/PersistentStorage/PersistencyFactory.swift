a8313eb93533bc154dfa385c91078e39357d34c0
rzmn <nrazumnyi@gmail.com>
2024-09-02 17:43:26 +0300

    modern concurrency data

--- client/iOS/Packages/Data/PersistentStorage/Sources/PersistentStorage/PersistencyFactory.swift
+++ client/iOS/Packages/Data/PersistentStorage/Sources/PersistentStorage/PersistencyFactory.swift
@@ -1,6 +1,6 @@
 import DataTransferObjects
 
-public protocol PersistencyFactory {
+public protocol PersistencyFactory: Sendable {
     func awake() async -> Persistency?
     func create(hostId: UserDto.ID, refreshToken: String) async throws -> Persistency
 }
