66721f73a7588df39af9a3ba7e1b7a46cb67266c
rzmn <nrazumnyi@gmail.com>
2024-11-19 16:32:40 +0300

    removed unused code

--- Verni/SceneDelegate.swift
+++ Verni/SceneDelegate.swift
@@ -1,13 +1,4 @@
 import UIKit
 import App
 
-class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-//    func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {
-//        guard let context = URLContexts.first else {
-//            return
-//        }
-//        Task {
-//            await app()?.handle(url: context.url.absoluteString)
-//        }
-//    }
-}
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {}
